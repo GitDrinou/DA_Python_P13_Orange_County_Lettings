ca3b2f29620e338edab7234d8fe16f9f94e32913
Sandrine BRIVAL <brival.sandrine@gmail.com>
2026-04-21 10:29:11 +0200

    refactor(admin): add verbose name on models

--- profiles/models.py
+++ profiles/models.py
@@ -10,5 +10,9 @@ class Profile(models.Model):
     )
     favorite_city = models.CharField(max_length=64, blank=True)
 
+    class Meta:
+        verbose_name = "Profile"
+        verbose_name_plural = "Profiles"
+
     def __str__(self):
         return self.user.username
