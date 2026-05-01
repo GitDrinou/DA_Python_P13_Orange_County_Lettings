7d7b276649757bb729ce5595e050af6127292e04
Sandrine BRIVAL <brival.sandrine@gmail.com>
2026-05-01 12:17:15 +0200

    styles(static): add staticfiles directory

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -179,4 +179,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static", ]
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
